2cb5e0b3ddd30b0c08779d31281e33635237670d
Earl Miles <earl.miles25@gmail.com>
2025-11-23 11:58:00 +0000

    First commit of 'Projects' repo

---
+++ Python programs/Shape_editor/Shape.py
@@ -0,0 +1,82 @@
+
+
+
+class Shape_():
+
+   
+
+    def __init__(self, name, height = 200, width = 200, length = 200, colour = "grey"):
+        super().__init__()
+
+        self.name = name
+        self.height = height
+        self.width = width
+        self.length = length
+        self.colour = colour
+
+    
+    def displayShape(self):
+        print("Name: " + self.name)
+        print("Height: " + str(self.height))
+        print("Width: " + str(self.width))
+        print("Length: " + str(self.length))
+        print("Colour: " + str(self.colour))
+
+
+
+class Cube(Shape_):
+    def __init__(self, name, height = 200, width = 200, length = 200, colour = "grey"):
+        super().__init__(name, height, width, length, colour)
+
+class Sphere(Shape_):
+    def __init__(self, name, radius, colour):
+        self.name = name
+        self.radius = radius
+        self.colour = colour
+
+    def displayShape(self):
+        print("Name: " + self.name)
+        print("Radius: " + str(self.radius))
+        print("Colour: " + str(self.colour))
+
+class Cone(Shape_):
+    def __init__(self, name, height = 200, width = 200, length = 200, colour = "grey"):
+        super().__init__(name, height, width, length, colour)
+
+class Cylinder(Shape_):
+    def __init__(self, name, height = 200, width = 200, length = 200, colour = "grey"):
+        super().__init__(name, height, width, length, colour)
+
+object_list = []
+
+def newCube(name, height, width, length, colour):
+
+    length = height = width
+
+    cube = Cube(name, height, width, length, colour)
+
+    object_list.append(cube)
+
+    cube.displayShape()
+
+
+def newSphere(name, radius, colour):
+
+    sphere = Sphere(name, radius, colour)
+
+    object_list.append(sphere)
+
+    sphere.displayShape()
+
+
+def viewShapes():
+
+    for obj in object_list:
+        print(f"{obj.name}")
+
+
+
+    
+
+
+    
---
+++ main.py
@@ -0,0 +1,23 @@
+class Car:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+
+
+    
+
+
+message = "Th"
+
+
+if len(message) > 7:
+    print("Hello")
+
+elif len(message) < 4:
+    print("Goodbye")
+
+
+
+
+    
