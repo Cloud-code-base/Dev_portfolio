1f1650afa2c768b6c91076b9725f8515633f5a26
Earl Miles <earl.miles25@gmail.com>
2025-11-24 11:06:06 +0000

    File change: Removed 'Transform.py' from Shape_editor folder

--- Python programs/Shape_editor/Transform.py
+++
@@ -1,9 +0,0 @@
-
-class Transform:
-    
-    def scale(obj, scaleFactor):
-        obj.width = obj.width * scaleFactor
-        obj.height = obj.height * scaleFactor
-        obj.length = obj.length * scaleFactor
-
-        obj.displayShape()
